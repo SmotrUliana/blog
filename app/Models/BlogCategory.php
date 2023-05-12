4dbca62b7e1150f6081fba9b6b4297512e292209
SmotrUliana <uliana.smotr@oa.edu.ua>
2023-05-12 03:14:27 +0300

    Laboratory 10

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class BlogCategory extends Model
 {
     use SoftDeletes;
+    const ROOT = 1;
     use HasFactory;
     protected $fillable
         = [
@@ -16,5 +17,45 @@ class BlogCategory extends Model
             'slug',
             'parent_id',
             'description',
+
         ];
+
+    /**
+     * Батьківська категорія
+     *
+     * @return BlogCategory
+     */
+    public function parentCategory()
+    {
+        //належить категорії
+        return $this->belongsTo(BlogCategory::class, 'parent_id', 'id');
+    }
+
+    /**
+     * Приклад аксесуара (Accessor)
+     *
+     * @url https://laravel.com/docs/7.x/eloquent-mutator
+     *
+     * @return string
+     */
+    public function getParentTitleAttribute()
+    {
+        $title = $this->parentCategory->title
+            ?? ($this->isRoot()
+                ? 'Корінь'
+                : '???');
+
+        return $title;
+    }
+
+    /**
+     * Перевірка чи об'єкт є кореневим
+     *
+     * @return bool
+     */
+    public function isRoot()
+    {
+        return $this->id === BlogCategory::ROOT;
+    }
+
 }
