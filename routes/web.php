848325687e2604edee97944a7e00377c77d63c16
SmotrUliana <uliana.smotr@oa.edu.ua>
2023-06-05 13:34:28 +0300

    Laboratory 13

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
-use App\Http\Controllers\DiggingDeeperController;
-use App\Http\Controllers\Blog\Admin\CategoryController;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\DiggingDeeperController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,20 +13,40 @@ use App\Http\Controllers\RestTestController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index','edit','store','update','create',];
+    Route::resource('categories', CategoryController::class)
+        ->only($methods)
+        ->names('blog.admin.categories');
+    //BlogPost
+    Route::resource('posts', PostController::class)
+        ->except(['show'])                               //не робити маршрут для метода show
+        ->names('blog.admin.posts');
 
-Route::get('/', function () {
-    return view('welcome');
 });
-
-
-Route::group(['prefix' => 'digging_deeper'], function () {
+Route::group(['prefix' => 'digging_deeper','namespace' => 'App\Http\Controllers'], function () {
 
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
 
         ->name('digging_deeper.collections');
 
-});
+    Route::get('process-video', 'DiggingDeeperController@processVideo')
+        ->name('digging_deeper.processVideo');
 
+    Route::get('prepare-catalog', 'DiggingDeeperController@prepareCatalog')
+        ->name('digging_deeper.prepareCatalog');
+
+
+
+});
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::middleware([
     'auth:sanctum',
@@ -40,22 +60,4 @@ Route::middleware([
 Route::resource('rest', RestTestController::class)->names('restTest');
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
-});
-
-//Адмінка
-$groupData = [
-    'namespace' => 'App\Http\Controllers\Blog\Admin',
-    'prefix' => 'admin/blog',
-];
-Route::group($groupData, function () {    //BlogPost
-    Route::resource('posts', PostController::class)
-        ->except(['show'])                               //не робити маршрут для метода show
-        ->names('blog.admin.posts');
-
-
-    //BlogCategory
-    $methods = ['index','edit','store','update','create',];
-    Route::resource('categories', CategoryController::class)
-        ->only($methods)
-        ->names('blog.admin.categories');
 });
